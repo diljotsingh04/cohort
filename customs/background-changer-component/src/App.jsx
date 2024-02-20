ed0952b4719a440713a7947a817de0fbc5a8bae3
Diljot Singh <singhdiljot2306@gmail.com>
2024-02-20 15:33:00 +0530

    added backgound changer component

---
+++ customs/background-changer-component/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './style.css'
+
+function App() {
+
+  const [bgColor, setBgColor] = useState("white");
+
+  return (
+    <div className='container' style={{ background: bgColor }}>
+      <div className="bottom">
+        <button onClick={() => setBgColor("red")} id='red'>Red</button>
+        <button onClick={() => setBgColor("green")} id='green'>Green</button>
+        <button onClick={() => setBgColor("blue")} id='blue'>Blue</button>
+        <button onClick={() => setBgColor("yellow")} id='yellow'>Yellow</button>
+        <button onClick={() => setBgColor("white")} id='white'>Default</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
