6d13037f6386c51e47781c15c09931e81908ccca
Diljot Singh <singhdiljot2306@gmail.com>
2024-03-19 12:16:55 +0530

    jwt

--- react-practice/basic-react-app/src/App.jsx
+++ react-practice/basic-react-app/src/App.jsx
@@ -1,70 +1,33 @@
 import { useState } from 'react';
-import TodoCard from './components/TodoCard';
+import jwt from 'jsonwebtoken';
 
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
 
 function App() {
-  const [list, setList] = useState([
-    {
-      id: 1,
-      title: "This is title 1", 
-      description: "This is description 1",
-      completed: false
-    },
-    {
-      id: 2,
-      title: "This is title 2", 
-      description: "This is description 2",
-      completed: true
-    },
-    {
-      id: 3,
-      title: "This is title 3", 
-      description: "This is description 3",
-      completed: true
-    }
-  ]);
+  const [decodedToken, setDecodedToken] = useState(null);
 
-  const[newTitle, setNewTitle] = useState("");
-  const[newDescription, setNewDescription] = useState("");
-
-  function submit(){
-    if(newTitle.length == 0 && newDescription == 0){
-      alert("Title and Description Required");
-      return;
+  // Verify the token
+  const verifyToken = () => {
+    try {
+      // Replace 'secret' with your actual secret key used for signing JWTs
+      const decoded = jwt.verify(token, 'secret');
+      console.log(decoded);
+      setDecodedToken(decoded);
+    } catch (error) {
+      console.error('Invalid token:', error.message);
     }
-    const newTodo = {
-      id: list.length === 0 ? 0 : list[list.length - 1].id + 1,
-      title: newTitle,
-      description: newDescription,
-      completed: false
-    }
-    setList([...list, newTodo]);
-    setNewTitle("");
-    setNewDescription("");
-  }
+  };
 
   return (
     <>
-      <Fields submit={submit} newTitle={newTitle} setNewTitle={setNewTitle} newDescription={newDescription} setNewDescription={setNewDescription}/>
-      <div>
-        {
-        list.map(todo => 
-          <TodoCard key={todo.id} id={todo.id} title={todo.title} description={todo.description} completed={todo.completed}/>
-        )
-        }
-      </div>
+      <button onClick={verifyToken}>Verify Token</button>
+      {decodedToken && (
+        <div>
+          Your json web token is: {JSON.stringify(decodedToken)}
+        </div>
+      )}
     </>
-  )
-}
-
-function Fields(props){
-  return (
-    <div>
-      <input type="text" id='titleValue' onChange={(e) => props.setNewTitle(e.target.value)} value={props.newTitle} placeholder='Enter the title'/>
-      <input type="text" id='descriptionValue' onChange={(e) => props.setNewDescription(e.target.value)} value={props.newDescription} placeholder='Enter the description'/>
-      <button onClick={props.submit}>Submit</button>
-    </div>
-  )
+  );
 }
 
 export default App;
